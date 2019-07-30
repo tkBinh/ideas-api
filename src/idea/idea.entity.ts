5a1dfdd69f383c1b10272534379b28f0699e1409
VietIS Bình <binh.hathanh@vietis.com.vn>
2019-07-30 17:15:32 +0700

    --------

---
+++ src/idea/idea.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('idea')
+export class IdeaEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @Column('text')
+  idea: string;
+
+  @Column('text')
+  description: string;
+}
